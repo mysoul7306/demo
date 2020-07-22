bce9c7600ed750becf6cb33cf52124ad5928d812
ROK_ROOT <45331778+mysoul7306@users.noreply.github.com>
2020-07-22 20:03:47 +0900

    Function added  - QueryDSL added

--- src/main/java/kr/co/company/persistence/board/BoardRepository.java
+++ src/main/java/kr/co/company/persistence/board/BoardRepository.java
@@ -1,12 +1,13 @@
 package kr.co.company.persistence.board;
 
 import kr.co.company.demo.board.vo.res.BoardDefaultResVO;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Collection;
 import java.util.List;
 
-public interface BoardRepository extends CrudRepository<BoardDefaultResVO, Long> {
+public interface BoardRepository extends CrudRepository<BoardDefaultResVO, Long>, QuerydslPredicateExecutor<BoardDefaultResVO> {
 	// find<Clazz>By<Column>
 	List<BoardDefaultResVO> findBoardDefaultResVOByTitle(String title);
 	List<BoardDefaultResVO> findBoardDefaultResVOSByContentsContains(String contents);
